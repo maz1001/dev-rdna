8c369dd6f16974c877720f12173e05f514b13dc4
maz1001 <43093013+maz1001@users.noreply.github.com>
2020-05-20 01:31:20 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -6,6 +6,29 @@
 	<meta name="viewport" content="initial-scale=1, maximum-scale=1, width=device-width">
     <title data-ng-bind="pageTitle + (pageTitle?' &mdash; ':'') + 'Rdna Store'">Rdna Store</title>
 
+    <!-- Tags -->
+    <?php
+    require_once __DIR__.'/php/seo/tags.php';
+    $tags = getTags();
+    if ($tags) {
+        echo '<meta name="title" content="'.$tags->title."\" />\n\t";
+        echo '<meta name="description" content="'.$tags->description."\" />\n";
+        echo '<meta property="og:title" content="'.$tags->title."\" />\n\t";
+        echo '<meta property="og:description" content="'.$tags->description."\" />\n";
+        echo '<meta property="og:type" content="'.$tags->type."\" />\n\t";
+        echo '<meta property="og:url" content="'.$tags->url."\" />\n\t";
+        echo '<meta property="og:image" content="'.$tags->image."\" />\n\t";
+        echo '<meta name="twitter:title" content="'.$tags->title."\" />\n\t";
+        echo '<meta name="twitter:description" content="'.$tags->description."\" />\n";
+        echo '<meta name="twitter:image" content="'.$tags->image."\" />\n\t";
+    }
+    ?>
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+    <meta name="author" content="Ordering.co">
+    <meta name="generator" content="Ordering.co">
+    <!-- End tags -->
+
     <link rel="stylesheet" type="text/css" href="assets/css/ionic.app.css">
     <script src="cordova.js"></script>
     <script src="assets/js/jquery.min.js"></script>
@@ -16,7 +39,6 @@
     <script src="assets/js/ua-parser.min.js"></script>
     <script src="https://js.stripe.com/v3/"></script>
     <script type="text/javascript" src="https://www.paypalobjects.com/api/checkout.min.js"></script>
-    <script charset="utf-8" type="text/javascript" src="https://js.hsforms.net/forms/v2.js"></script>
 
     <!--<base href="/">-->
     <meta name="description" content="Rdna أرضنا is a speciality grocery store & a community food initiative."><meta name="keywords" content="grocery, organic, local, butcher,  vegan,  vegetarian">
